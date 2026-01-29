a8edc44978f4aa22003d42e873f0e2053d0e8edc
Abdow <abdowm99@gmail.com>
2026-01-29 17:15:15 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'abdowthesia-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon.png',
+  'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
+];
+
+// Install the service worker and cache everything
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
